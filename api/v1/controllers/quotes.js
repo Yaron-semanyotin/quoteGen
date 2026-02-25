473a1b29383efe0772a130f6ce9b54222dc2784c
Yaron Semanyotin <126324yaron@gmail.com>
2026-02-25 10:36:11 +0200

    puppeteer file path added

--- api/v1/controllers/quotes.js
+++ api/v1/controllers/quotes.js
@@ -254,7 +254,11 @@ const quotesCtrl = {
       });
 
       // ומחכה שכל התוכן והמשאבים ייטענו לתוך דף html וטוען את ה chromium פותח
-      const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+      const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: puppeteer.executablePath(),
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+      });
       const page = await browser.newPage();
 
       await page.setContent(html, { waitUntil: ['domcontentloaded', 'networkidle0'] }); // אומר אין יותר בקשות רשת  networkidle0 אומר הדומ נטען domcontentloaded
---
+++ puppeteerrc.cjs
@@ -0,0 +1,9 @@
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // חשוב: לשמור את הדפדפן בתוך הפרויקט כדי שייכנס ל-deploy artifact
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
